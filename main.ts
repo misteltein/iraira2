212bb6eade87dd4c29218e9430909d38cbaa460d
Itoga, Hibiki <itogahibiki@gmail.com>
2020-08-01 11:03:44 +0900

    WIP

--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
-let gameover = 0
-let goal = 0
-let score = 0
 input.onButtonPressed(Button.AB, function () {
     control.reset()
     start()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (0 == goal && 0 == gameover) {
+        gameover = 1
+        music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+        basic.showIcon(IconNames.No)
+    }
 })
 function start () {
     gameover = 0
     goal = 0
     score = 100
+    basic.showIcon(IconNames.Yes)
 }
+let score = 0
+let gameover = 0
+let goal = 0
+start()
 basic.forever(function () {
-	
+    if (0 == goal && 0 < score) {
+        score += -1
+    }
+    basic.pause(500)
 })
